98a4904fb21ddfecfbfbce7d60ed3764996673c3
dpkrobomad <dpkrobomad@gmail.com>
2025-03-29 03:38:51 +0000

    change

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -428,6 +428,8 @@ export default function Dashboard() {
         </div>
       </div>
 
+
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <SensorWidget
           title="pH"
